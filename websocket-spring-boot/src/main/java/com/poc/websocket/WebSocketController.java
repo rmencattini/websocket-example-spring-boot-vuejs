8457832469ac889e53303b57c39693bb569635a1
Romain Mencattini <romain.mencattini@evooq.ch>
2023-07-05 15:10:12 +0200

    Commit two folders

---
+++ websocket-spring-boot/src/main/java/com/poc/websocket/WebSocketController.java
@@ -0,0 +1,20 @@
+package com.poc.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class WebSocketController {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public void greeting(String message) {
+        messagingTemplate.convertAndSend("/topic/greetings", "Hello, " + message + "!");
+        messagingTemplate.convertAndSend("/alert/trigger", "");
+    }
+}
---
+++ websocket-vue/src/App.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import * as Stomp from "@stomp/stompjs";
+
+const result = ref<string>("");
+const connected = ref<boolean>(false);
+const name = ref<string>("");
+const shake = ref<boolean>(false);
+
+function sendGrettings(): void {
+  stompClient.publish({destination: '/app/hello', body: name.value})
+}
+
+const stompClient = new Stomp.Client({
+  brokerURL: 'ws://localhost:7100/websocket',
+  onConnect() {
+      connected.value = true;
+      stompClient.subscribe('/topic/greetings', (message: Stomp.IMessage) => {
+        result.value = message.body;
+      });
+      stompClient.subscribe('/alert/trigger', () => {
+        shake.value = true;
+        setTimeout(() => {
+          shake.value = false;
+        }, 2000)
+      })
+  }
+});
+
+stompClient.activate();
+
+</script>
+
+<template>
+  <div>
+    <img src="/vite.svg" class="logo" :class="{shake: shake}" alt="Vite logo" />
+    <img src="./assets/vue.svg" class="logo vue" :class="{shake: shake}" alt="Vue logo" />
+  </div>
+  <div v-if="!connected">Waiting for websocket connection</div>
+  <button v-else class="button" type="submit" @click="sendGrettings()">Send grettings to: </button><input v-model="name" placeholder="Name to be greeted">
+  <br>
+  <br>
+  Results: {{  result }}
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.button {
+  margin-right: 2rem;
+}
+
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-2px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(4px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-8px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(8px, 0, 0);
+  }
+}
+</style>
